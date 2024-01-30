9ee95764811b9a1f8abc337a802eba77ffa3f5f4
manoj-mj2001 <108316542+manoj-mj2001@users.noreply.github.com>
2024-01-30 13:31:14 +0530

    task 01 done

---
+++ Level-2_Task-1_Calculator/script.js
@@ -0,0 +1,51 @@
+let currentDisplay = '0';
+let resultDisplay = false;
+const clicksound = new Audio('./ClickSound.mpeg');
+function appendToDisplay(value) {
+  if (currentDisplay === '0' || resultDisplay) {
+    currentDisplay = value;
+  } else {
+    currentDisplay += value;
+  }
+  resultDisplay = false;
+  clicksound.play();
+  updateDisplay();
+}
+
+function updateDisplay() {
+  clicksound.play();
+  const displayElement = document.getElementById('display');
+  displayElement.textContent = currentDisplay;
+}
+
+function calculateResult() {
+  try {
+    const result = eval(currentDisplay);
+    currentDisplay += '\n =' + result.toString();
+    updateDisplay();
+  } catch (error) {
+    currentDisplay += '\nError';
+    updateDisplay();
+  }
+
+  resultDisplay = true;
+}
+
+function clearLastElement() {
+  currentDisplay = currentDisplay.slice(0, -1);
+  if (currentDisplay === '') {
+    currentDisplay = '0';
+  }
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentDisplay = '0';
+  updateDisplay();
+}
+
+// Attach handleOverflow to window resize event
+window.addEventListener('resize', handleOverflow);
+
+// Call handleOverflow initially to handle any overflow on page load
+handleOverflow();
